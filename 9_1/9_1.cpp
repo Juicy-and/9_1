adc52dfb6664b5a73e7ebcb36fa82b9675a99cc1
Juicy-and <131388626+Juicy-and@users.noreply.github.com>
2023-06-13 20:57:03 +0300

    Update 9_1.cpp

--- 9_1/9_1.cpp
+++ 9_1/9_1.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 
 class Fraction
 {
@@ -6,14 +6,40 @@ private:
 	int numerator_;
 	int denominator_;
 
+	void cut_frac(int& numerator_, int& denominator_) {
+		int num = 0;
+		if (numerator_ < 0)
+		{
+			numerator_ *= -1;
+			num++;
+		}
+		int nod = NOD(numerator_, denominator_);
+
+		numerator_ /= nod;
+		denominator_ /= nod;
+		if (num) numerator_ *= -1;
+	}
+	int NOD(int numerator_, int denominator_) {
+		while (numerator_ > 0 && denominator_ > 0) {
+			if (numerator_ > denominator_) {
+				numerator_ %= denominator_;
+			}
+			else {
+				denominator_ %= numerator_;
+			}
+		}
+
+		return numerator_ + denominator_;
+	}
 public:
 	Fraction(int numerator, int denominator)
 	{
 		numerator_ = numerator;
 		denominator_ = denominator;
+		cut_frac(numerator_, denominator_);
 	}
 	double Abs() { return std::sqrt(numerator_ * numerator_ + denominator_ * denominator_); }
-	bool operator== (Fraction other) { return Abs() == other.Abs(); }
+	bool operator==(Fraction other) { return Abs() == other.Abs(); }
 	bool operator!=(Fraction other) { return !(*this == other); }
 	bool operator<(Fraction other) { return other > *this; }
 	bool operator>(Fraction other) { return other.Abs() > Abs(); }
@@ -24,8 +50,7 @@ public:
 int main()
 {
 	Fraction f1(4, 3);
-	Fraction f2(6, 11);
-
+	Fraction f2(8, 6);
 	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
 	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
 	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
